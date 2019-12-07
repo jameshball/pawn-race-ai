94942e2de42c74ca868bd8e40a696b5231680b73
James Ball <balljames63@gmail.com>
2019-12-07 23:56:20 +0000

    Added example of issue and todos.

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -43,37 +43,6 @@ public class TestSuite {
 
   @Test
   public void unmakeMoveTest() {
-    Board board = new Board();
-    Move forwardWhite = new Move(new Vector(6, 0), new Vector(5, 0));
-    Move doubleForwardWhite = new Move(new Vector(6, 0), new Vector(4, 0));
-
-    assertTrue("Forward move not valid.",
-      board.isValidMove(forwardWhite));
-    assertTrue("Double forward move not valid.",
-      board.isValidMove(doubleForwardWhite));
-
-    board.makeHumanMove("a4");
-
-    Move forwardBlack = new Move(new Vector(1, 0), new Vector(2, 0));
-    Move doubleForwardBlack = new Move(new Vector(1, 0), new Vector(3, 0));
-
-    assertTrue("Forward move not valid.",
-      board.isValidMove(forwardBlack));
-    assertTrue("Double forward move not valid.",
-      board.isValidMove(doubleForwardBlack));
-
-    board.makeHumanMove("b5");
-
-    Move diagonalCaptureWhite = new Move(new Vector(4, 0), new Vector(3, 1));
-
-    assertTrue("Diagonal capture not valid.",
-      board.isValidMove(diagonalCaptureWhite));
-
-    board.makeHumanMove("g3");
-
-    Move diagonalCaptureBlack = new Move(new Vector(3, 1), new Vector(4, 0));
-
-    assertTrue("Diagonal capture not valid.",
-      board.isValidMove(diagonalCaptureBlack));
+    // TODO: Implement.
   }
 }
